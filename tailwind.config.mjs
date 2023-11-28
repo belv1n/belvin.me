a7426d4246fa5ff788883d50101edc82928ca1d2
Yonipy <kelvinh39@nycstudents.net>
2023-11-28 18:59:32 -0500

    pre-commits of the website

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				"space": ['Space Grotesk', 'sans-serif'],
+				"jet": ['JetBrains Mono', 'monospace'],
+			},
+		}
+	},
+	plugins: [],
+}
