9da311e0f4b5be5f8cd056adb7d6c055e74cef5a
Yonipy <kelvinh39@nycstudents.net>
2023-11-30 20:25:40 -0500

    The website looks great credits to afn.im for inspo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,21 @@ export default {
 				"space": ['Space Grotesk', 'sans-serif'],
 				"jet": ['JetBrains Mono', 'monospace'],
 			},
+			keyframes: {
+				fadein: {
+					'0%': { transform: 'scale(0.8, 0.8)' },
+					'100%': { transform: 'scale(1, 1)' },
+				  },
+				levitate: {
+					'0%': { transform: 'translateY(0px)' },
+					'50%': { transform: 'translateY(-10px)' },
+					'100%': { transform: 'translateY(0px)' },
+				}
+			},
+			animation: {
+				fadein: 'fadein .3s ease-in-out',
+				levitate: 'levitate 3.5s ease-in-out infinite',
+			}
 		}
 	},
 	plugins: [],
