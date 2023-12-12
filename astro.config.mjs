a5cfd92a08a4d16c7d7d2cdc4bc4f63ed6ba39f4
Yonipy <kelvinh39@nycstudents.net>
2023-12-11 20:05:34 -0500

    added some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false,
-  }
-    
-  )]
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
